7babae8eb37a19967ec3fde11b6cab750a7e8a0f
Robert Monden <dev@robertmonden.com>
2018-06-22 10:01:51 +0200

    CSS changes

--- App.js
+++ App.js
@@ -26,46 +26,57 @@ export default class App extends React.Component {
                         marginBottom: 30,
                         textAlign: 'center'
                     }}>HiLang</Text>
-                    <TextInput  label="E-mail address"
-                                placeholder="somebody@example.com"
-                                value={ this.state.email }
-                                onChangeText={ email => this.setState({email}) }
-                                style={{
-                                    margin: 30,
-                                    padding: 15,
-                                    fontSize: 25,
-                                    borderWidth: 1,
-                                    borderColor: '#efefef',
-                                    backgroundColor: '#fff',
-                                    letterSpacing: -1
-                                }}></TextInput>
+                    <View style={{
+                        backgroundColor: 'rgba(250, 250, 250, 0.8)',
+                        margin: 15
+                    }}>
+                        <TextInput  label="E-mail address"
+                                    placeholder="somebody@example.com"
+                                    value={ this.state.email }
+                                    onChangeText={ email => this.setState({email}) }
+                                    style={{
+                                        margin: 30,
+                                        padding: 15,
+                                        fontSize: 25,
+                                        borderWidth: 1,
+                                        borderColor: '#efefef',
+                                        backgroundColor: '#fff',
+                                        letterSpacing: -1
+                                    }}></TextInput>
 
-                    <TextInput  label="Password"
-                                onKeyPress={ (keyPress) => console.log(keyPress)}
-                                onChangeText={ password => this.setState({password}) }
-                                style={{
-                                    marginLeft: 30,
-                                    marginRight: 30,
-                                    marginTop: 0,
-                                    padding: 15,
-                                    fontSize: 25,
-                                    borderWidth: 1,
-                                    borderColor: '#efefef',
-                                    backgroundColor: '#fff',
-                                    letterSpacing: -1
-                                }}
-                                secureTextEntry={true}
-                                onSubmitEditing= { () => {
+                        <TextInput  label="Password"
+                                    onKeyPress={ (keyPress) => console.log(keyPress)}
+                                    onChangeText={ password => this.setState({password}) }
+                                    style={{
+                                        marginLeft: 30,
+                                        marginRight: 30,
+                                        marginTop: 0,
+                                        marginBottom: 30,
+                                        padding: 15,
+                                        fontSize: 25,
+                                        borderWidth: 1,
+                                        borderColor: '#efefef',
+                                        backgroundColor: '#fff',
+                                        letterSpacing: -1
+                                    }}
+                                    secureTextEntry={true}
+                                    onSubmitEditing= { () => {
+                                        Alert.alert('email: ' + this.state.email, "pw: " + this.state.password);
+                                    }}>
+                        </TextInput>
+
+                        <View style={{
+                            backgroundColor: 'rgba(215, 215, 215, 0.8)',
+                            padding: 10
+                        }}>
+                            <Button
+                                onPress={() => {
                                     Alert.alert('email: ' + this.state.email, "pw: " + this.state.password);
-                                }}>
-                    </TextInput>
-
-                    <Button
-                        onPress={() => {
-                            Alert.alert('email: ' + this.state.email, "pw: " + this.state.password);
-                        }}
-                        title="Log in"
-                        />
+                                }}
+                                title="Log in"
+                            />
+                        </View>
+                    </View>
                 </View>
             </ImageBackground>
         );
