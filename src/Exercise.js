84ae23f3975064b16bbc15770ecb0fbf06a591b2
TheElmo <jelmer.internet@xs4all.nl>
2018-06-26 15:42:46 +0200

    start exercise class

---
+++ src/Exercise.js
@@ -0,0 +1,53 @@
+import { View, StyleSheet, Text, TouchableOpacity, ScrollView, TextInput, Alert } from 'react-native';
+import styles from '../assets/css/Style';
+import Api from './Api.js';
+
+export default class Exercise{
+    constructor(){
+        this.vocabulary = [];
+        this.currentWord = null;
+        this.incorrectWords = [];
+        this.correctWords = [];
+        this.answer = "";
+    }
+
+    innitialize(lesson_id) {
+        Api.getInstance().callApi('api/lesson/' + lesson_id, 'POST', {}, response => {
+            this.totalLength = response.vocabulary.length;
+            for(question of response.vocabulary) {
+                this.vocabulary.push({
+                    id: question.id,
+                    question: question.translation,
+                    correctAnswer: question.native,
+                    sentenceStructure: question.sentenceStructure,
+                    lesson_id: question.lesson_id
+                });
+            }
+            this.currentWord = this.vocabulary[0];
+        });
+    }
+
+    next(answer) {
+        if(answer === this.currentWord.correctAnswer) {
+            this.correctWords.push(this.currentWord);
+        } else {
+            this.incorrectWords.push(this.currentWord);
+        }
+        if(this.hasNext()) {
+            this.vocabulary.shift();
+        } else {
+        	this.vocabulary = this.incorrectWords.slice();
+        }
+
+        this.currentWord = this.vocabulary[0];
+    }
+
+    hasNext() {
+        return this.vocabulary.length > 1;
+    }
+
+    getCurrentWord() {
+    	return this.currentWord;
+    }
+
+}
