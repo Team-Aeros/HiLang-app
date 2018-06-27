e8a39d30b6d0fd2ef97b2d229412e4364e34edbc
Robert Monden <dev@robertmonden.com>
2018-06-27 14:33:52 +0200

    Logout functionality

---
+++ src/navigation/AccountAreaNavigation.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import styles from '../../assets/css/Style.js';
+
+export default class AccountAreaNavigation extends React.Component {
+
+    static navigationOptions = {
+        title: 'Browse courses'
+    };
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            navigationItems: [
+                {
+                    label: 'View profile',
+                    href: '',
+                    params: {}
+                },
+                {
+                    label: 'My courses',
+                    href: '',
+                    params: {}
+                },
+                {
+                    label: 'Logout',
+                    href: 'Logout',
+                    params: {}
+                }
+            ]
+        };
+    }
+
+    render() {
+        return (
+            <View>
+                {this.state.navigationItems.map((item, key) => {
+                    return (
+                        <TouchableOpacity style={styles.simple_list_item} onPress={() => this.props.navigation.navigate(item.href, item.params)} key={key}>
+                            <Text>{ item.label }</Text>
+                        </TouchableOpacity>
+                    );
+                })}
+            </View>
+        );
+    }
+}
